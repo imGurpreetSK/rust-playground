758bd41b3d04d5268c8e98af10c376a6885881a8
Gurpreet Singh <gurpreetsk@hotmail.com>
2024-08-26 15:17:02 -0700

    Introduce variable.

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,7 @@
 fn main() {
+    let mut sum = 0;
     for i in 0..10 {
-        if i % 2 == 0 { println!("Even {}", i) } else { println!("Odd {}", i) }
+        sum += i;
     }
+    println!("Sum is {}", sum)
 }
