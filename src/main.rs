495f3be11502629c131df5f5113cb9525ca7afff
Gurpreet Singh <gurpreetsk@hotmail.com>
2024-08-26 17:47:06 -0700

    Need to revisit: referencing https://stevedonovan.github.io/rust-gentle-intro/1-basics.html

--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,6 @@
 fn main() {
-    println!("Factorial is {}", factorial(5));
-}
-
-fn factorial(n: u64) -> u64 {
-   if n == 0 {
-       1
-   } else {
-       n * factorial(n - 1)
-   }
+    let x = 3;
+    println!("First: {}", x);
+    println!("Second: {}", &x);
+    println!("Third: {}", *&x);
 }
