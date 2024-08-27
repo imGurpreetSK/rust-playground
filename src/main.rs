a37056ac5a1d7f39b02e12abe07275193f4228a5
Gurpreet Singh <gurpreetsk@hotmail.com>
2024-08-26 18:53:22 -0700

    More arrays, slices and borrowing.

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,10 @@
 fn main() {
     // Arrays are always homogenous.
-    let array1: [i32; 7] = [1, 2, 3, 4, 5, 6, 7];
-    let array2: [[i32; 3]; 2] = [[1, 2, 3], [3, 4, 5]];
-    let array3: [[&str; 1]; 2] = [["One"], ["Two"]];
-    println!("array1 is {:?}", array1); // _Debug_ printing.
-    println!("array2 is {:?}", array2); // _Debug_ printing.
-    println!("array3 is {:?}", array3) // _Debug_ printing.
+    let array: [i32; 7] = [1, 2, 3, 4, 5, 6, 7];
+    let slice1: &[i32] = &array[0..2];
+    let slice2: &[i32] = &array[2..7];
+
+    println!("array is {:?}", array);
+    println!("slice1 is {:?}", slice1); // Slices are *always* borrowed. A copy is never made.
+    println!("slice2 is {:?}", slice2) // Very intimate relationship with arrays.
 }
