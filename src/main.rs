b657c05248780e3ee71055cc5c266cf5cdff80fa
Gurpreet Singh <gurpreetsk@hotmail.com>
2024-08-26 18:45:07 -0700

    Slices and borrowing.

--- src/main.rs
+++ src/main.rs
@@ -1,13 +1,12 @@
 fn main() {
-    /*
-     * Arrays are not used that often in Rust, because the type of array includes its size.
-     * The type of the array in the example is [i32; 4]; the type of [10, 20] would be [i32; 2] and so forth: they have different types.
-     * So they are bastards to pass around as function arguments.
-     *
-     * Though, due to this property, rust knows the size of array at compile time. Hence, ArrayIndexOutOfBounds don't happen.
-     */
-    let array: [i32; 5] = [1, 2, 3, 4, 5];
-    for i in 0..array.len() {
-        println!("[{i}]: {} ", array[i])
+    let array: [i32; 7] = [1, 2, 3, 4, 5, 6, 7];
+    println!("Sum is {}", sum(&array)) // Use '&' to pass slice to function --> Borrowing.
+}
+
+fn sum(slice: &[i32]) -> i32 { // Notice slice's type.
+    let mut sum = 0;
+    for i in 0..slice.len() {
+        sum += slice[i];
     }
+    return sum
 }
