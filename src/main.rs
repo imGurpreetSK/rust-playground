94f58ac51f63f03fb6654b94cb523e5b5099176b
Gurpreet Singh <gurpreetsk@hotmail.com>
2024-08-26 18:49:17 -0700

    Arrays are always homogenous; debug printing.

--- src/main.rs
+++ src/main.rs
@@ -1,12 +1,9 @@
 fn main() {
-    let array: [i32; 7] = [1, 2, 3, 4, 5, 6, 7];
-    println!("Sum is {}", sum(&array)) // Use '&' to pass slice to function --> Borrowing.
-}
-
-fn sum(slice: &[i32]) -> i32 { // Notice slice's type.
-    let mut sum = 0;
-    for i in 0..slice.len() {
-        sum += slice[i];
-    }
-    return sum
+    // Arrays are always homogenous.
+    let array1: [i32; 7] = [1, 2, 3, 4, 5, 6, 7];
+    let array2: [[i32; 3]; 2] = [[1, 2, 3], [3, 4, 5]];
+    let array3: [[&str; 1]; 2] = [["One"], ["Two"]];
+    println!("array1 is {:?}", array1); // _Debug_ printing.
+    println!("array2 is {:?}", array2); // _Debug_ printing.
+    println!("array3 is {:?}", array3) // _Debug_ printing.
 }
