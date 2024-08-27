671c2eff7f553135de889e20ffb11dfd888bbdee
Gurpreet Singh <gurpreetsk@hotmail.com>
2024-08-26 17:38:10 -0700

    Use unsigned int.

--- src/main.rs
+++ src/main.rs
@@ -1,18 +1,11 @@
 fn main() {
-    println!("Square is {}", square(2.0));
-    println!("Clamped: {}", clamp(3.0, 5.0, 7.0));
+    println!("Factorial is {}", factorial(5));
 }
 
-fn clamp(x: f64, lower: f64, upper: f64) -> f64 {
-    if x < lower {
-        lower
-    } else if x > upper {
-        upper
-    } else {
-        x
-    }
-}
-
-fn square(x: f64) -> f64 {
-   x * x
+fn factorial(n: u64) -> u64 {
+   if n == 0 {
+       1
+   } else {
+       n * factorial(n - 1)
+   }
 }
