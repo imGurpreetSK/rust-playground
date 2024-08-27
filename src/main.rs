feaade613d504048c7de8c3be860fc432d14f30d
Gurpreet Singh <gurpreetsk@hotmail.com>
2024-08-26 17:35:02 -0700

    Work with function types.

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,18 @@
 fn main() {
-    let mut sum = 0.0;
-    for i in 0..10 {
-        sum += i as f32;
+    println!("Square is {}", square(2.0));
+    println!("Clamped: {}", clamp(3.0, 5.0, 7.0));
+}
+
+fn clamp(x: f64, lower: f64, upper: f64) -> f64 {
+    if x < lower {
+        lower
+    } else if x > upper {
+        upper
+    } else {
+        x
     }
-    println!("Sum is {}", sum)
+}
+
+fn square(x: f64) -> f64 {
+   x * x
 }
