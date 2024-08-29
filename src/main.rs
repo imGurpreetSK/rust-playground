bc7c0b5b624b0231ffa9bd5d607c37818a6a0c41
Gurpreet Singh <gurpreetsk@hotmail.com>
2024-08-29 14:09:24 -0700

    Command line arguments and expect().

--- src/main.rs
+++ src/main.rs
@@ -1,25 +1,6 @@
-fn dump(s: &str) {
-    println!("str '{}'", s);
-}
-
-// Read: https://stevedonovan.github.io/rust-gentle-intro/1-basics.html#strings
 fn main() {
-    let text = "hello dolly";  // the string slice
-    let s = text.to_string();  // it's now an allocated string
-    let _string = String::new();
-
-    dump(text);
-    dump(&s);
-
-    let text = "the red fox and the lazy dog";
-    let output: String = text.chars()
-        .filter(|x| { !x.is_whitespace() })
-        .collect();
-    println!("{output}");
-
-    for i in text.chars() {
-        if !i.is_whitespace() {
-            print!("{i}");
-        }
-    }
+    // Don't get comfortable with panic.
+    let arg = std::env::args().nth(1).expect("no arguments supplied");
+    let i = arg.parse::<i32>().expect("not an integer!");
+    println!("{i}")
 }
