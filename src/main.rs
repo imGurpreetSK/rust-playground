d68e0cdd42afb3b69ad691916b29d73363b89bec
Gurpreet Singh <gurpreetsk@hotmail.com>
2024-08-31 11:38:34 -0700

    Strict scoping and borrowing - no dangling pointers.

--- src/main.rs
+++ src/main.rs
@@ -1,27 +1,10 @@
 fn main() {
-    // https://stevedonovan.github.io/rust-gentle-intro/1-basics.html#matching
-    let multilingual = "Hi! ¡Hola! привет!";
-
-    match multilingual.find("п") {
-        None => { println!("Couldn't find the greeting; Hello!") }
-        Some(index) => {
-            let hi = &multilingual[index..];
-            println!("Russian hi: {}", hi)
-        }
+    // https://stevedonovan.github.io/rust-gentle-intro/2-structs-enums-lifetimes.html
+    let s1 = "Hello dolly".to_string();
+    let mut rs1 = &s1;
+    {
+        let tmp = "hello world".to_string();
+        rs1 = &tmp;
     }
-
-    // If only interested in one of the possible results.
-    if let Some (index) = multilingual.find("п") {
-        println!("Russian hi: {}", &multilingual[index..])
-    }
-
-    let n = 8;
-    println!(
-        "{}",
-        match n {
-            0..=3 => "small",
-            4..=6 => "medium",
-            _ => "large"
-        }
-    )
+    println!("{}", rs1)
 }
