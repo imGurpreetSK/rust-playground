49c5266d9e4a54d577849059cdf28b999e25af82
Gurpreet Singh <gurpreetsk@hotmail.com>
2024-08-31 12:15:11 -0700

    More methods, moving, modifications.

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,21 @@
 fn main() {
     let person = Person::new("Gurpreet", "Singh");
-    println!("{}", person.full_name())
+    println!("{}", person.full_name());
+
+    let mut new = person.copy();
+    new.set_first_name("Swaran");
+
+    let t = new.to_tuple();
+    println!("{} {}", t.0, t.1);
+    println!("{} {}", new.first_name, new.last_name) // Value has moved; error.
 }
+
+/*
+    - no self argument: you can associate functions with structs, like the new "constructor".
+    - &self argument: can use the values of the struct, but not change them
+    - &mut self argument: can modify the values
+    - self argument: will consume the value, which will move.
+*/
 
 struct Person {
     first_name: String,
@@ -17,7 +31,19 @@ impl Person {
         }
     }
 
+    fn copy(&self) -> Person {
+        Self::new(&self.first_name, &self.last_name)
+    }
+
     fn full_name(&self) -> String {
         format!("{} {}", self.first_name, self.last_name)
+    }
+
+    fn set_first_name(&mut self, name: &str) {
+        self.first_name = name.to_string()
+    }
+
+    fn to_tuple(self) -> (String, String) {
+        (self.first_name, self.last_name)
     }
 }
