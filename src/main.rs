9813bb14de66faa28cb9fda3ed68a9949a7990da
Gurpreet Singh <gurpreetsk@hotmail.com>
2024-08-31 11:48:02 -0700

    Getting started with structs and associated functions.

--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,18 @@
 fn main() {
-    // https://stevedonovan.github.io/rust-gentle-intro/2-structs-enums-lifetimes.html
-    let s1 = "Hello dolly".to_string();
-    let mut rs1 = &s1;
-    {
-        let tmp = "hello world".to_string();
-        rs1 = &tmp;
+    let person = Person::new("Gurpreet", "Singh");
+    println!("{} {}", person.first_name, person.last_name)
+}
+
+struct Person {
+    first_name: String,
+    last_name: String,
+}
+
+impl Person {
+    fn new(first: &str, last: &str) -> Person {
+        Person {
+            first_name: first.to_string(),
+            last_name: last.to_string(),
+        }
     }
-    println!("{}", rs1)
 }
