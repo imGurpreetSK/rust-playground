9a84a54ccbce69284b82ff86ed6529311a4f1d7d
Gurpreet Singh <gurpreetsk@hotmail.com>
2024-09-04 19:50:08 -0700

    Read user input from stdin.

--- guessing_name/src/main.rs
+++ guessing_name/src/main.rs
@@ -1,3 +1,15 @@
+use std::io;
+
 fn main() {
-    println!("Hello, world!");
+    println!("=============== Guess the number! ===============");
+
+    println!("Please input your guess");
+
+    let mut input = String::new();
+
+    io::stdin()
+        .read_line(&mut input)
+        .expect("Failed to read line :(");
+
+    println!("You guessed: {}", input)
 }
