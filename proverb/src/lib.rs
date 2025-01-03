5745f4bf51512b938ee279d035fd7650fbb8a76f
Gurpreet Singh <gurpreetsk@hotmail.com>
2025-01-02 16:45:47 -0800

    Solve proverb - better version.

--- proverb/src/lib.rs
+++ proverb/src/lib.rs
@@ -1,17 +1,11 @@
 pub fn build_proverb(list: &[&str]) -> String {
-    if list.is_empty() {
-        return String::new();
+    match list.first() {
+        None => { "".to_string() }
+        Some(word) => {
+            list.windows(2)
+                .map(|pair| { format!("For want of a {} the {} was lost.\n", pair[0], pair[1]) })
+                .chain(std::iter::once(format!("And all for the want of a {}.", word)))
+                .collect()
+        }
     }
-
-    let mut verses: Vec<String> = vec![];
-    for i in 1..list.len() {
-        verses.push(format!(
-            "For want of a {} the {} was lost.",
-            list[i - 1],
-            list[i]
-        ));
-    }
-    verses.push(format!("And all for the want of a {}.", list[0]));
-
-    verses.join("\n")
 }
