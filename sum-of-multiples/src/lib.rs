5d27d13f8a0546ec554b2ed914c4a69b41e26228
Gurpreet Singh <gurpreetsk@hotmail.com>
2025-01-03 15:45:54 -0800

    Solve sum-of-multiples using flatmap.

--- sum-of-multiples/src/lib.rs
+++ sum-of-multiples/src/lib.rs
@@ -1,10 +1,10 @@
 use std::collections::HashSet;
 
 pub fn sum_of_multiples(limit: u32, factors: &[u32]) -> u32 {
-    let mut set = HashSet::new();
-    factors.iter().filter(|x| **x != 0).for_each(|factor| {
-        (*factor..limit).filter(|x| x % factor == 0).for_each(|x| { set.insert(x); });
-    });
-
-    set.iter().sum::<u32>()
+    factors.iter()
+        .filter(|&&factor| { factor != 0 })
+        .flat_map(|&factor| { (factor..limit).step_by(factor as usize) })
+        .collect::<HashSet<u32>>()
+        .iter()
+        .sum()
 }
